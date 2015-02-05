0c4b56abe1a61f452b75077cd4a3f51859b0fced
tiredtyrant <tiredtyrant@gmail.com>
2015-02-05 17:45:46 -0200

    Gravando flairs no banco
    Signed-off-by: tiredtyrant <tiredtyrant@gmail.com>

--- import_flairs.py
+++ import_flairs.py
@@ -14,8 +14,8 @@ flair_list = subreddit.get_flair_list(limit=None)
 count = 0
 for item in flair_list:
     count += 1
-    print item
-    cur.execute('INSERT INTO flairs (uf,username,cidade) VALUES (\'%s\',\'%s\',\'%s\');'%(item['flair_css_class'].encode('utf-8'),item['user'].encode('utf-8'),item['flair_text'].encode('utf-8')))
+    query = 'INSERT INTO flairs (uf,username,cidade) VALUES (\'%s\',\'%s\',\'%s\');'%(item['flair_css_class'],item['user'],item['flair_text'])
+    cur.execute(query)
 
 con.commit()
 print count
