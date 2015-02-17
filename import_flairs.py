b1da539d3c596c37c2a62bdb1b2c68b85582a739
tiredtyrant <tiredtyrant@gmail.com>
2015-02-17 18:37:56 -0200

    Alterado script de importacao de flairs para nao duplicar usuarios
    Signed-off-by: tiredtyrant <tiredtyrant@gmail.com>

--- import_flairs.py
+++ import_flairs.py
@@ -15,10 +15,13 @@ flair_list = subreddit.get_flair_list(limit=None)
 count = 0
 for item in flair_list:
     count += 1
-    cur.execute('INSERT INTO flairs (uf,username,cidade) VALUES (?,?,?);',(item['flair_css_class'],item['user'],item['flair_text']))
+    if cur.execute('SELECT Count(*) FROM flairs WHERE username == ?',(item['user'],)).fetchone()[0]:
+        cur.execute('UPDATE flairs SET username=?,uf=?,cidade=? WHERE username==?;',(item['user'],item['flair_css_class'],item['flair_text'],item['user']))
+    else:
+        cur.execute('INSERT INTO flairs (uf,username,cidade) VALUES (?,?,?);',(item['flair_css_class'],item['user'],item['flair_text']))
 
 time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
-if cur.execute('select Count(*) from lastupdate;').fetchone()[0]:
+if cur.execute('SELECT Count(*) FROM lastupdate;').fetchone()[0]:
     cur.execute('UPDATE lastupdate SET id=1,lastdatetime=? WHERE id=1;',(time,))
 else:
     cur.execute('INSERT INTO lastupdate (id, lastdatetime) VALUES (1,?);',(time,))
