ed981cb1c4e259d7e5e1064d5eb68d34228cc0fc
tiredtyrant <tiredtyrant@gmail.com>
2015-02-17 19:42:44 -0200

    Script de importacao alterado para repetir periodicamente
    Signed-off-by: tiredtyrant <tiredtyrant@gmail.com>

--- import_flairs.py
+++ import_flairs.py
@@ -1,6 +1,7 @@
 import praw
 import sqlite3
 import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 r = praw.Reddit('user-agent')
 
@@ -8,24 +9,31 @@ r.login('user','password')
 
 subreddit = r.get_subreddit('brasil')
 
-con = sqlite3.connect('flairs.db')
-cur = con.cursor()
+def importflairs():
+    con = sqlite3.connect('flairs.db')
+    cur = con.cursor()
+    flair_list = subreddit.get_flair_list(limit=None)
+    count = 0
+    for item in flair_list:
+        count += 1
+        if cur.execute('SELECT Count(*) FROM flairs WHERE username == ?',(item['user'],)).fetchone()[0]:
+            cur.execute('UPDATE flairs SET username=?,uf=?,cidade=? WHERE username==?;',(item['user'],item['flair_css_class'],item['flair_text'],item['user']))
+        else:
+            cur.execute('INSERT INTO flairs (uf,username,cidade) VALUES (?,?,?);',(item['flair_css_class'],item['user'],item['flair_text']))
 
-flair_list = subreddit.get_flair_list(limit=None)
-count = 0
-for item in flair_list:
-    count += 1
-    if cur.execute('SELECT Count(*) FROM flairs WHERE username == ?',(item['user'],)).fetchone()[0]:
-        cur.execute('UPDATE flairs SET username=?,uf=?,cidade=? WHERE username==?;',(item['user'],item['flair_css_class'],item['flair_text'],item['user']))
+    time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
+    if cur.execute('SELECT Count(*) FROM lastupdate;').fetchone()[0]:
+        cur.execute('UPDATE lastupdate SET id=1,lastdatetime=? WHERE id=1;',(time,))
     else:
-        cur.execute('INSERT INTO flairs (uf,username,cidade) VALUES (?,?,?);',(item['flair_css_class'],item['user'],item['flair_text']))
+        cur.execute('INSERT INTO lastupdate (id, lastdatetime) VALUES (1,?);',(time,))
 
-time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
-if cur.execute('SELECT Count(*) FROM lastupdate;').fetchone()[0]:
-    cur.execute('UPDATE lastupdate SET id=1,lastdatetime=? WHERE id=1;',(time,))
-else:
-    cur.execute('INSERT INTO lastupdate (id, lastdatetime) VALUES (1,?);',(time,))
+    con.commit()
+    print time
+    print count
 
-con.commit()
-print time
-print count
+scheduler = BlockingScheduler()
+scheduler.add_job(importflairs, 'interval', hours=3)
+try:
+    scheduler.start()
+except(KeyboardInterrupt,SystemExit):
+    pass
